b6a0ce36c7f082b9f07e34238729394a5c030ef4
Umar Jamil <umar.jamil@img-advisors.com>
2020-08-03 16:46:00 +0800

    Routes Updated

--- routes/post.js
+++ routes/post.js
@@ -4,6 +4,16 @@ const mongoose = require('mongoose')
 const requireLogin = require('../middleware/requireLogin')
 const Post = mongoose.model("Post")
 
+router.get('/allpost', (req, res) => {
+    Post.find()
+        .populate("postedBy", "_id name")
+        .then(posts => {
+            res.json({ posts })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
 
 router.post('/createpost', requireLogin, (req, res) => {
     const { title, body, pic } = req.body
@@ -19,6 +29,17 @@ router.post('/createpost', requireLogin, (req, res) => {
     })
     post.save().then(result => {
             res.json({ post: result })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+router.get('/mypost', requireLogin, (req, res) => {
+    Post.find({ postedBy: req.user._id })
+        .populate("PostedBy", "_id name")
+        .then(mypost => {
+            res.json({ mypost })
         })
         .catch(err => {
             console.log(err)
